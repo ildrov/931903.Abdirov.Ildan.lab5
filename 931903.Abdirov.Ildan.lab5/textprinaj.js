aa55f2621a53a9922dabcd85b722d728598dfec6
ildrov <81831225+ildrov@users.noreply.github.com>
2021-10-23 16:27:30 +0700

    Add files via upload

---
+++ 931903.Abdirov.Ildan.lab5/textprinaj.js
@@ -0,0 +1,30 @@
+﻿
+textprinaj1.onclick = function () {
+    vspl1.style.display = "block"
+    bg1.style.display = "block"
+}
+textprinaj2.onclick = function () {
+    vspl2.style.display = "block"
+    bg2.style.display = "block"
+}
+textprinaj3.onclick = function () {
+    vspl3.style.display = "block"
+    bg3.style.display = "block"
+}
+
+
+
+vspl1.onclick = function () {
+    vspl1.style.display = "none"
+    bg1.style.display = "none"
+}
+vspl2.onclick = function () {
+    vspl2.style.display = "none"
+    bg2.style.display = "none"
+}
+vspl3.onclick = function () {
+    vspl3.style.display = "none"
+    bg3.style.display = "none"
+}
+
+
